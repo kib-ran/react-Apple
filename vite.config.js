7fef8fb6810ff069e3b958931d3b488950a6e15b
kibran meng <kibran29mengistu@gmail.com>
2025-10-25 12:03:26 +0300

    Fix broken import and missing module

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,26 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server:{
-//     port:3000
-//   }
-
-// })
-
-
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // 👈 This is the key for Render deployment
-  server: {
-    port: 3000,
-  },
-});
+  server:{
+    port:3000
+  }
+
+})
+
+
+
+
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "./", // 👈 This is the key for Render deployment
+//   server: {
+//     port: 3000,
+//   },
+// });
